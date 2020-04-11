d859261cf9a0864640f2af542da7e9065f0d8c78
Andrea Catalini <andrea.catalini89@gmail.com>
2020-04-11 17:16:59 +0200

    Escluded modifiers from counting CPS

--- src/main_window/view.rs
+++ src/main_window/view.rs
@@ -3,7 +3,7 @@ use gtk::prelude::*;
 use gtk::*;
 use gtk::{Window, WidgetExt, Inhibit, WindowType};
 use relm_derive::Msg;
-use gdk::{EventKey, keyval_to_unicode};
+use gdk::{EventKey, keyval_to_unicode, enums::key};
 use relm_derive::{widget};
 
 use super::model::Model;
@@ -24,11 +24,32 @@ impl Widget for Win {
     }
 
     fn update(&mut self, event: Msg) {
+        let chars_to_skip = vec![ key::Tab, key::Caps_Lock,
+                            key::Shift_L, key::Shift_R,
+                            key::Control_L, key::Control_R,  
+                            key::Super_L, key::Super_R, key::Alt_L, key::Alt_R,
+                            key::Num_Lock, key::Return, key::BackSpace ];
         match event {
             Msg::Flush => {
                 self.model.flush_cps();
             }
-            Msg::KeyPress(event) => self.model.key_pressed(),
+            Msg::KeyPress(event) => {
+                let mut to_skip = false;
+                for curr_char in chars_to_skip {
+                    if event.get_keyval() == curr_char {
+
+                        #[cfg(debug_assertions)]
+                        {
+                            println!("Skipped! ;)");
+                        }
+                        to_skip = true;
+                        break;
+                    }
+                }
+                if !to_skip {
+                    self.model.key_pressed();
+                }
+            },
             Msg::KeyDown => self.model.key_pressed(),
             Msg::Quit => gtk::main_quit(),
         }
