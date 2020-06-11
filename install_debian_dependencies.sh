d59a1436ee7b7a680c6a9ba2a6763fef963fbbec
Andrea Catalini <andrea.catalini89@gmail.com>
2020-06-11 17:15:45 +0200

    First look at different GUI framework

--- install_debian_dependencies.sh
+++ install_debian_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# if you came here looking to fullfill dependencies for a fedora based OSes,
+# if you came here looking to fullfill dependencies for fedora based OSes,
 # a search with yum or dnf should do. Good luck!
 
-sudo apt install -y libgtk-3-dev
+# sudo apt install -y libgtk-3-dev
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,61 @@
-mod main_window;
-mod plot_widget;
+use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text};
 
-use relm::Widget;
-use main_window::view::Win;
+mod char_per_sec;
+//use char_per_sec::CPS;
 
+pub fn main() {
+    Counter::run(Settings::default())
+    // CPS::run(Settings::default())
+}
 
-fn main() {
-    Win::run(()).unwrap();
+#[derive(Default)]
+struct Counter {
+    value: i32,
+    increment_button: button::State,
+    decrement_button: button::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .padding(20)
+            .align_items(Align::Center)
+            .push(
+                Button::new(&mut self.increment_button, Text::new("Increment"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(Text::new(self.value.to_string()).size(50))
+            .push(
+                Button::new(&mut self.decrement_button, Text::new("Decrement"))
+                    .on_press(Message::DecrementPressed),
+            )
+            .into()
+    }
 }
